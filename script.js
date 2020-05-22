544cf7e72c0ea55b9025bf402370cc11d9ded159
msi-hub <52900333+msi-hub@users.noreply.github.com>
2020-05-22 16:07:46 +1200

    Update_01

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+$(function () {
+    var formHandler = function (e) {
+      e.preventDefault(); // Avoid form submit
+      var fromuser = $(this).serializeArray();
+      var inputName =  fromuser[0].value;
+      console.log(inputName); //need to send this value to forniteApi [HELP]
+    };
+    $('form').submit(formHandler);
+});
+/*
 var fortniteApi = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://fortnite-api.p.rapidapi.com/stats/Sniper%2520OCE",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fortnite-api.p.rapidapi.com",
-		"x-rapidapi-key": "9022f20635msh069f3a96f55ebeep14a836jsn7571baeacb94"
-	}
+  "async": true,
+  "crossDomain": true,
+  "url": 'https://fortnite-api.p.rapidapi.com/stats/'+inputName,
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
+    "x-rapidapi-key": "9022f20635msh069f3a96f55ebeep14a836jsn7571baeacb94"
+  }
 }
-
 $.ajax(fortniteApi).done(function (data) {
   console.log(data);
-	console.log(data.user.displayName + " " + data.season.keyboardmouse.all.winrate);
+  console.log("Player Name:"+ data.user.displayName); //need to send this value to displayName [HELP]
 });
+*/
+//var displayName = data.user.displayName;
