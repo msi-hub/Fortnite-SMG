838f4552b1f27287ae94f41a272db2e4d6fae635
msi-hub <52900333+msi-hub@users.noreply.github.com>
2020-06-08 13:58:28 +1200

    Update_04

---
+++ data.js
@@ -0,0 +1,49 @@
+var ffa_Name = localStorage.getItem("faccount");
+
+var fss_Outlived = parseFloat(localStorage.getItem("ss_Outlived"));
+var fss_Winrate = parseFloat(localStorage.getItem("ss_Winrate"));
+var fss_Matches = parseFloat(localStorage.getItem("ss_Outlived"));
+var fss_Kills = parseFloat(localStorage.getItem("ss_Kills"));
+var fss_Top25 = parseFloat(localStorage.getItem("ss_Top25"));
+var fss_Top10 = parseFloat(localStorage.getItem("ss_Top10"));
+var fss_Top01 = parseFloat(localStorage.getItem("ss_Top01"));
+
+var fls_Outlived = parseFloat(localStorage.getItem("ls_Outlived"));
+var fls_Winrate = parseFloat(localStorage.getItem("ls_Winrate"));
+var fls_Matches = parseFloat(localStorage.getItem("ls_Outlived"));
+var fls_Kills = parseFloat(localStorage.getItem("ls_Kills"));
+var fls_Top25 = parseFloat(localStorage.getItem("ls_Top25"));
+var fls_Top10 = parseFloat(localStorage.getItem("ls_Top10"));
+var fls_Top01 = parseFloat(localStorage.getItem("ls_Top01"));
+
+var fsd_Outlived = parseFloat(localStorage.getItem("sd_Outlived"));
+var fsd_Winrate = parseFloat(localStorage.getItem("sd_Winrate"));
+var fsd_Matches = parseFloat(localStorage.getItem("sd_Outlived"));
+var fsd_Kills = parseFloat(localStorage.getItem("sd_Kills"));
+var fsd_Top12 = parseFloat(localStorage.getItem("sd_Top12"));
+var fsd_Top05 = parseFloat(localStorage.getItem("sd_Top05"));
+var fsd_Top01 = parseFloat(localStorage.getItem("sd_Top01"));
+
+var fld_Outlived = parseFloat(localStorage.getItem("ld_Outlived"));
+var fld_Winrate = parseFloat(localStorage.getItem("ld_Winrate"));
+var fld_Matches = parseFloat(localStorage.getItem("ld_Outlived"));
+var fld_Kills = parseFloat(localStorage.getItem("ld_Kills"));
+var fld_Top12 = parseFloat(localStorage.getItem("ld_Top12"));
+var fld_Top05 = parseFloat(localStorage.getItem("ld_Top05"));
+var fld_Top01 = parseFloat(localStorage.getItem("ld_Top01"));
+
+var fsq_Outlived = parseFloat(localStorage.getItem("sq_Outlived"));
+var fsq_Winrate = parseFloat(localStorage.getItem("sq_Winrate"));
+var fsq_Matches = parseFloat(localStorage.getItem("sq_Outlived"));
+var fsq_Kills = parseFloat(localStorage.getItem("sq_Kills"));
+var fsq_Top06 = parseFloat(localStorage.getItem("sq_Top06"));
+var fsq_Top03 = parseFloat(localStorage.getItem("sq_Top03"));
+var fsq_Top01 = parseFloat(localStorage.getItem("sq_Top01"));
+
+var flq_Outlived = parseFloat(localStorage.getItem("lq_Outlived"));
+var flq_Winrate = parseFloat(localStorage.getItem("lq_Winrate"));
+var flq_Matches = parseFloat(localStorage.getItem("lq_Outlived"));
+var flq_Kills = parseFloat(localStorage.getItem("lq_Kills"));
+var flq_Top06 = parseFloat(localStorage.getItem("lq_Top06"));
+var flq_Top03 = parseFloat(localStorage.getItem("lq_Top03"));
+var flq_Top01 = parseFloat(localStorage.getItem("lq_Top01"));
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function () {
       e.preventDefault(); // Avoid form submit
       var fromuser = $(this).serializeArray();
       var inputName =  fromuser[0].value;
-      //rapidApi(inputName);
+      rapidApi(inputName);
     };
     $('form').submit(formHandler);
 
@@ -26,108 +26,211 @@ function rapidApi(inputName) {
 }
 
 function fromApi(data){ //fromApi(data)
-/*
-  player_Name = data.user.displayName;
-  //timeline_mode
-  s_Solo  = data.season.all.defaultsolo;
-  l_Solo  = data.lifetime.all.defaultsolo;
-  s_Duo   = data.season.all.defaultduo;
-  l_Duo   = data.lifetime.all.defaultduo;
-  s_Squad = data.season.all.defaultsquad;
-  l_Squad = data.lifetime.all.defaultsquad;
-  //season_solo_Statistics
-  var s_solo_Outlived = s_Solo.playersoutlived;
-  var s_solo_Winrate  = s_Solo.winrate;
-  var s_solo_Matches  = s_Solo.matchesplayed;
-  var s_solo_Score    = s_Solo.score;
-  var s_solo_Kills    = s_Solo.kills;
-  var s_solo_Top25    = s_Solo.placetop25;
-  var s_solo_Top10    = s_Solo.placetop10;
-  var s_solo_Top01    = s_Solo.placetop1;
-  //lifetime_solo_Statistics
-  var l_solo_Outlived = l_Solo.playersoutlived;
-  var l_solo_Winrate  = l_Solo.winrate;
-  var l_solo_Matches  = l_Solo.matchesplayed;
-  var l_solo_Score    = l_Solo.score;
-  var l_solo_Kills    = l_Solo.kills;
-  var l_solo_Top25    = l_Solo.placetop25;
-  var l_solo_Top10    = l_Solo.placetop10;
-  var l_solo_Top01    = l_Solo.placetop1;
-  //season_duo_Statistics
-  var s_duo_Outlived = s_Duo.playersoutlived;
-  var s_duo_Winrate  = s_Duo.winrate;
-  var s_duo_Matches  = s_Duo.matchesplayed;
-  var s_duo_Score    = s_Duo.score;
-  var s_duo_Kills    = s_Duo.kills;
-  var s_duo_Top25    = s_Duo.placetop12;
-  var s_duo_Top10    = s_Duo.placetop5;
-  var s_duo_Top01    = s_Duo.placetop1;
-  //lifetime_duo_Statistics
-  var l_duo_Outlived = l_Duo.playersoutlived;
-  var l_duo_Winrate  = l_Duo.winrate;
-  var l_duo_Matches  = l_Duo.matchesplayed;
-  var l_duo_Score    = l_Duo.score;
-  var l_duo_Kills    = l_Duo.kills;
-  var l_duo_Top25    = l_Duo.placetop12;
-  var l_duo_Top10    = l_Duo.placetop5;
-  var l_duo_Top01    = l_Duo.placetop1;
-  //season_squad_Statistics
-  var s_squad_Outlived = s_Squad.playersoutlived;
-  var s_squad_Winrate  = s_Squad.winrate;
-  var s_squad_Matches  = s_Squad.matchesplayed;
-  var s_squad_Score    = s_Squad.score;
-  var s_squad_Kills    = s_Squad.kills;
-  var s_squad_Top25    = s_Squad.placetop6;
-  var s_squad_Top10    = s_Squad.placetop3;
-  var s_squad_Top01    = s_Squad.placetop1;
-  //lifetime_squad_Statistics
-  var l_squad_Outlived = l_Squad.playersoutlived;
-  var l_squad_Winrate  = l_Squad.winrate;
-  var l_squad_Matches  = l_Squad.matchesplayed;
-  var l_squad_Score    = l_Squad.score; //
-  var l_squad_Kills    = l_Squad.kills;
-  var l_squad_Top25    = l_Squad.placetop6; //
-  var l_squad_Top10    = l_Squad.placetop3; //
-  var l_squad_Top01    = l_Squad.placetop1; //
-  */
-  //test
 
-  var t_Name = "Ninja";
-  var s_t_Outlived = 1232;
-  var s_t_Winrate  = 54.5;
-  var s_t_Matches  = 5321;
-  var s_t_Score    = 2151;
-  var s_t_Kills    = 5213;
-  var s_t_Top25    = 12;
-  var s_t_Top10    = 54;
-  var s_t_Top01    = 4;
-  var l_t_Outlived = 214123;
-  var l_t_Winrate  = 34.5;
-  var l_t_Matches  = 5323121;
-  var l_t_Score    = 2123151;
-  var l_t_Kills    = 5252513;
-  var l_t_Top25    = 212;
-  var l_t_Top10    = 5244;
-  var l_t_Top01    = 244;
-  //console.log(player_Name);
-  //console.log(data);
-  return [
-    t_Name, //0
-    s_t_Outlived, //1
-    s_t_Winrate,   //2
-    s_t_Matches ,    //3
-    s_t_Score,   //4
-    s_t_Kills,    //5
-    s_t_Top25,    //6
-    s_t_Top10,     //7
-    s_t_Top01,     //8
-    l_t_Outlived ,  //9
-    l_t_Winrate,    //10
-    l_t_Matches,   //11
-    l_t_Score,   //12
-    l_t_Kills,    //13
-    l_t_Top25,    //14
-    l_t_Top10,      //15
-    l_t_Top01 ];   //16
+  if (data==null)
+  {
+    console.log("Hello World");
+  }
+  else {
+    document.getElementById('err').style.visibility = "visible";
+
+    if(data.user.displayName != null){
+      window.open("gensmap.html","_self");
+
+      document.getElementById('err').style.visibility = "hidden";
+
+      var player_Name = data.user.displayName;
+      console.log(data);
+      console.log(player_Name);
+      //timeline_mode_checknull
+      if (data.season.all.defaultsolo !=null){
+        var s_solo = data.season.all.defaultsolo;
+      } else {
+        var s_solo = 0;
+      }
+      if (data.lifetime.all.defaultsolo !=null){
+        var l_solo = data.lifetime.all.defaultsolo;
+      } else {
+        var l_solo = 0;
+      }
+      if (data.season.all.defaultduo !=null){
+        var s_duo = data.season.all.defaultduo;
+      } else {
+        var s_duo = 0;
+      }
+      if (data.lifetime.all.defaultduo !=null){
+        var l_duo = data.lifetime.all.defaultduo;
+      } else {
+        var l_duo = 0;
+      }
+      if (data.season.all.defaultsquad !=null){
+        var s_squad = data.season.all.defaultsquad;
+      } else {
+        var s_squad = 0;
+      }
+      if (data.lifetime.all.defaultsquad !=null){
+        var l_squad = data.lifetime.all.defaultsquad;
+      } else {
+        var l_squad = 0;
+      }
+      //season_solo_Statistics
+      if (s_solo == 0){
+        var s_solo_Outlived = 0;
+        var s_solo_Winrate  = 0;
+        var s_solo_Matches  = 0;
+        var s_solo_Kills    = 0;
+        var s_solo_Top25    = 0;
+        var s_solo_Top10    = 0;
+        var s_solo_Top01    = 0;
+      } else {
+        var s_solo_Outlived = s_solo.playersoutlived;
+        var s_solo_Winrate  = s_solo.winrate;
+        var s_solo_Matches  = s_solo.matchesplayed;
+        var s_solo_Kills    = s_solo.kills;
+        var s_solo_Top25    = s_solo.placetop25;
+        var s_solo_Top10    = s_solo.placetop10;
+        var s_solo_Top01    = s_solo.placetop1;
+      }
+      //lifetime_solo_Statistics
+      if (l_solo == 0){
+        var l_solo_Outlived = 0;
+        var l_solo_Winrate  = 0;
+        var l_solo_Matches  = 0;
+        var l_solo_Kills    = 0;
+        var l_solo_Top25    = 0;
+        var l_solo_Top10    = 0;
+        var l_solo_Top01    = 0;
+      } else {
+        var l_solo_Outlived = l_solo.playersoutlived;
+        var l_solo_Winrate  = l_solo.winrate;
+        var l_solo_Matches  = l_solo.matchesplayed;
+        var l_solo_Kills    = l_solo.kills;
+        var l_solo_Top25    = l_solo.placetop25;
+        var l_solo_Top10    = l_solo.placetop10;
+        var l_solo_Top01    = l_solo.placetop1;
+      }
+
+      //season_duo_Statistics
+      if (s_duo == 0){
+        var s_duo_Outlived = 0;
+        var s_duo_Winrate  = 0;
+        var s_duo_Matches  = 0;
+        var s_duo_Kills    = 0;
+        var s_duo_Top12    = 0;
+        var s_duo_Top05    = 0;
+        var s_duo_Top01    = 0;
+      } else {
+        var s_duo_Outlived = s_duo.playersoutlived;
+        var s_duo_Winrate  = s_duo.winrate;
+        var s_duo_Matches  = s_duo.matchesplayed;
+        var s_duo_Kills    = s_duo.kills;
+        var s_duo_Top12    = s_duo.placetop12;
+        var s_duo_Top05    = s_duo.placetop5;
+        var s_duo_Top01    = s_duo.placetop1;
+      }
+      //lifetime_duo_Statistics
+      if (l_duo == 0){
+        var l_duo_Outlived = 0;
+        var l_duo_Winrate  = 0;
+        var l_duo_Matches  = 0;
+        var l_duo_Kills    = 0;
+        var l_duo_Top12    = 0;
+        var l_duo_Top05    = 0;
+        var l_duo_Top01    = 0;
+      } else {
+        var l_duo_Outlived = l_duo.playersoutlived;
+        var l_duo_Winrate  = l_duo.winrate;
+        var l_duo_Matches  = l_duo.matchesplayed;
+        var l_duo_Kills    = l_duo.kills;
+        var l_duo_Top12    = l_duo.placetop12;
+        var l_duo_Top05    = l_duo.placetop5;
+        var l_duo_Top01    = l_duo.placetop1;
+      }
+      //season_squad_Statistics
+      if (s_squad == 0){
+        var s_squad_Outlived = 0;
+        var s_squad_Winrate  = 0;
+        var s_squad_Matches  = 0;
+        var s_squad_Kills    = 0;
+        var s_squad_Top06    = 0;
+        var s_squad_Top03    = 0;
+        var s_squad_Top01    = 0;
+      } else {
+        var s_squad_Outlived = s_squad.playersoutlived;
+        var s_squad_Winrate  = s_squad.winrate;
+        var s_squad_Matches  = s_squad.matchesplayed;
+        var s_squad_Kills    = s_squad.kills;
+        var s_squad_Top06    = s_squad.placetop6;
+        var s_squad_Top03    = s_squad.placetop3;
+        var s_squad_Top01    = s_squad.placetop1;
+      }
+      //lifetime_squad_Statistics
+      if (l_squad == 0){
+        var l_squad_Outlived = 0;
+        var l_squad_Winrate  = 0;
+        var l_squad_Matches  = 0;
+        var l_squad_Kills    = 0;
+        var l_squad_Top06    = 0;
+        var l_squad_Top03    = 0;
+        var l_squad_Top01    = 0;
+      } else {
+        var l_squad_Outlived = l_squad.playersoutlived;
+        var l_squad_Winrate  = l_squad.winrate;
+        var l_squad_Matches  = l_squad.matchesplayed;
+        var l_squad_Kills    = l_squad.kills;
+        var l_squad_Top06    = l_squad.placetop6;
+        var l_squad_Top03    = l_squad.placetop3;
+        var l_squad_Top01    = l_squad.placetop1;
+      }
+    }
+    localStorage.setItem("faccount",player_Name);
+
+    localStorage.setItem("ss_Outlived",s_solo_Outlived);
+    localStorage.setItem("ss_Winrate",s_solo_Winrate);
+    localStorage.setItem("ss_Matches",s_solo_Outlived);
+    localStorage.setItem("ss_Kills",s_solo_Kills);
+    localStorage.setItem("ss_Top25",s_solo_Top25);
+    localStorage.setItem("ss_Top10",s_solo_Top10);
+    localStorage.setItem("ss_Top01",s_solo_Top01);
+
+    localStorage.setItem("ls_Outlived",l_solo_Outlived);
+    localStorage.setItem("ls_Winrate",l_solo_Winrate);
+    localStorage.setItem("ls_Matches",l_solo_Outlived);
+    localStorage.setItem("ls_Kills",l_solo_Kills);
+    localStorage.setItem("ls_Top25",l_solo_Top25);
+    localStorage.setItem("ls_Top10",l_solo_Top10);
+    localStorage.setItem("ls_Top01",l_solo_Top01);
+
+    localStorage.setItem("sd_Outlived",s_duo_Outlived);
+    localStorage.setItem("sd_Winrate",s_duo_Winrate);
+    localStorage.setItem("sd_Matches",s_duo_Outlived);
+    localStorage.setItem("sd_Kills",s_duo_Kills);
+    localStorage.setItem("sd_Top12",s_duo_Top12);
+    localStorage.setItem("sd_Top05",s_duo_Top05);
+    localStorage.setItem("sd_Top01",s_duo_Top01);
+
+    localStorage.setItem("ld_Outlived",l_duo_Outlived);
+    localStorage.setItem("ld_Winrate",l_duo_Winrate);
+    localStorage.setItem("ld_Matches",l_duo_Outlived);
+    localStorage.setItem("ld_Kills",l_duo_Kills);
+    localStorage.setItem("ld_Top12",l_duo_Top12);
+    localStorage.setItem("ld_Top05",l_duo_Top05);
+    localStorage.setItem("ld_Top01",l_duo_Top01);
+
+    localStorage.setItem("sq_Outlived",s_squad_Outlived);
+    localStorage.setItem("sq_Winrate",s_squad_Winrate);
+    localStorage.setItem("sq_Matches",s_squad_Outlived);
+    localStorage.setItem("sq_Kills",s_squad_Kills);
+    localStorage.setItem("sq_Top06",s_squad_Top06);
+    localStorage.setItem("sq_Top03",s_squad_Top03);
+    localStorage.setItem("sq_Top01",s_squad_Top01);
+
+    localStorage.setItem("lq_Outlived",l_squad_Outlived);
+    localStorage.setItem("lq_Winrate",l_squad_Winrate);
+    localStorage.setItem("lq_Matches",l_squad_Outlived);
+    localStorage.setItem("lq_Kills",l_squad_Kills);
+    localStorage.setItem("lq_Top06",l_squad_Top06);
+    localStorage.setItem("lq_Top03",l_squad_Top03);
+    localStorage.setItem("lq_Top01",l_squad_Top01);
+  }
 }
-var fromApidata = fromApi();
